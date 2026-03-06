a6bf069dc4cb0ccd0151222d7f37340cbc56eb66
Ganesh Kulakarni <ganeshkulakarni124@gmail.com>
2026-03-06 14:46:37 +0530

    adedd up layout.ejs file

--- Node.js/page34.js
+++ Node.js/page34.js
@@ -1,10 +1,22 @@
 import express from "express";
+import session from "express-session";
+import expressLayouts from 'express-ejs-layouts'
 const app = express();
 app.set("view engine", "ejs");
 app.set("views", "views");
-app.listen(5000, () => console.log("Server Started"));
+app.use(expressLayouts)
+app.use(express.static("public"))
+app.listen(5005, () => console.log("Server Started"));
+app.set("layout","layout")
 
 app.use(express.urlencoded({ extended: true }));
+app.use(
+  session({
+    secret: "mySecretKey",
+    resave: false,
+    saveUninitialized: false,
+  }),
+);
 
 const users = [
   { name: "Gaensh", email: "ganesh123@gmail.com", password: "1234" },
@@ -21,11 +33,13 @@ app.post("/login", (req, res) => {
   const user = users.find((user) => user.email === email);
   if (user) {
     if (user.password === password) {
+      req.session.user = user; //session id gets created, chcek in browser,inspect -application - cookies
       res.redirect("/");
     } else {
       res.redirect("/login");
     }
   } else {
+    alert("Make sure the Password")
     res.redirect("/login");
   }
   res.redirect("/");
@@ -36,21 +50,29 @@ app.get("/register", (req, res) => {
 });
 
 app.post('/register', (req, res) => {
-  const { email,password,name } = req.body;
-  const user= users.find((user) => user.email === email);
-  if(user){
+  const { email, password, name } = req.body;
+  const user = users.find((user) => user.email === email);
+  if (user) {
     res.redirect("/login");
-    
+
     return
   }
-  else{
+  else {
     users.push({
-      name: name,password:password,email:email});
+      name: name, password: password, email: email
+    });
     res.redirect("/login");
 
   }
 
 })
 app.get("/", (req, res) => {
-  res.render("dashboard", { users });
+  if (req.session.user) {
+    res.render("dashboard", { users });
+
+  }
+  else {
+    res.redirect('/login')
+  }
+
 });
